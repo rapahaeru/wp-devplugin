ca0eecabc7bc20019ca5442e66ac0f167853b69f
cael <rapahaeru@gmail.com>
2015-01-20 11:48:41 -0200

    Criação da pasta do plugin e seu arquivo de cabeçalho

---
+++ wp-content/plugins/ex-nivel1/ex-nivel1.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin Name: Exemplos nivel 1
+ * Plugin URI: http://link.onde.seu.plugin.esta.hospedado
+ * Description: Este plugin é apenas um exemplo do nivel 1 do curso de criação de plugins
+ * Version: 1.0
+ * Author: rapahaeru
+ * Author URI: http://github.com/rapahaeru
+ */
